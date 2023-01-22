3c0de36534cd9345351266a1f206bc8325a47ab9
Jesse  Tochukwu <71229278+HeDevedUp@users.noreply.github.com>
2023-01-22 15:18:02 -0700

    feat: added types

--- src/utils/motion.tsx
+++ src/utils/motion.tsx
@@ -46,7 +46,7 @@ export const staggerContainer = (staggerChildren: any, delayChildren: any) => ({
   },
 });
 
-export const textVariant = (delay) => ({
+export const textVariant = (delay: any) => ({
   hidden: {
     y: 50,
     opacity: 0,
@@ -87,7 +87,7 @@ export const textVariant2 = {
   },
 };
 
-export const fadeIn = (direction, type, delay, duration) => ({
+export const fadeIn = (direction: any, type: any, delay: any, duration: any) => ({
   hidden: {
     x: direction === 'left' ? 100 : direction === 'right' ? -100 : 0,
     y: direction === 'up' ? 100 : direction === 'down' ? -100 : 0,
@@ -106,7 +106,7 @@ export const fadeIn = (direction, type, delay, duration) => ({
   },
 });
 
-export const planetVariants = (direction) => ({
+export const planetVariants = (direction: any) => ({
   hidden: {
     x: direction === 'left' ? '-100%' : '100%',
     rotate: 120,
@@ -122,7 +122,7 @@ export const planetVariants = (direction) => ({
   },
 });
 
-export const zoomIn = (delay, duration) => ({
+export const zoomIn = (delay: any, duration: any) => ({
   hidden: {
     scale: 0,
     opacity: 0,
