d216b9cc2601528a0176c3a2f5147aa5b669c251
Jesse  Tochukwu <71229278+HeDevedUp@users.noreply.github.com>
2023-01-23 03:01:54 +0000

    Pending changes exported from your codespace

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,8 @@
-import {Link,Navbar, Text,useTheme } from "@nextui-org/react";
+import { Dropdown,Link, Navbar, Text, useTheme } from "@nextui-org/react";
 
-import  MyStyledButton  from "@/components/buttons/Button"
+import MyStyledButton from "@/components/buttons/Button"
+
+import { icons } from "@/asserts/Icons/icons";
 
 import { Layout } from "./Layout";
 
@@ -9,21 +11,16 @@ export default function NavbarItem() {
   const { theme } = useTheme();
 
   const collapseItems = [
-    "Profile",
-    "Dashboard",
-    "Activity",
-    "Analytics",
-    "System",
-    "Deployments",
-    "My Settings",
-    "Team Settings",
-    "Help & Feedback",
-    "Log Out",
+    "About Us",
+    "Products",
+    "FAQs",
+    "Sign In",
+    "Sign Out",
   ];
 
   return (
     <Layout>
-      <Navbar isBordered variant="sticky">
+      <Navbar isBordered variant="floating">
         <Navbar.Toggle showIn="xs" />
         <Navbar.Brand
           css={{
@@ -43,26 +40,112 @@ export default function NavbarItem() {
           hideIn="xs"
           variant="highlight"
         >
-          <Navbar.Link href="#">Trading Jornal</Navbar.Link>
-          <Navbar.Link href="#">Price Alert</Navbar.Link>
-          <Navbar.Link href="#"> Trader Tool </Navbar.Link>
-          <Navbar.Link href="#">Account Reporting</Navbar.Link>
-        </Navbar.Content>
-        <Navbar.Collapse>
-        {collapseItems.map((item, index) => (
-          <Navbar.CollapseItem key={item}>
-            <Link
-              color="inherit"
+
+          <Navbar.Link  href="#">
+            About Us
+          </Navbar.Link>
+
+
+          <Dropdown isBordered>
+            <Navbar.Item>
+              <Dropdown.Button
+                auto
+                light
+                css={{
+                  px: 0,
+                  dflex: "center",
+                  svg: { pe: "none" },
+                  w:'0px'
+                }}
+                iconRight={icons.chevron}
+                ripple={false}
+              >
+                Features
+              </Dropdown.Button>
+            </Navbar.Item>
+            <Dropdown.Menu
+              aria-label="   PsyDTrader features"
               css={{
-                minWidth: "100%",
+                $$dropdownMenuWidth: "340px",
+                $$dropdownItemHeight: "70px",
+                "& .nextui-dropdown-item": {
+                  py: "$4",
+                  // dropdown item left icon
+                  svg: {
+                    color: "$secondary",
+                    mr: "$4",
+                  },
+                  // dropdown item title
+                  "& .nextui-dropdown-item-content": {
+                    w: "100%",
+                    fontWeight: "$semibold",
+                  },
+                },
               }}
-              href="#"
             >
-              {item}
-            </Link>
-          </Navbar.CollapseItem>
-        ))}
-      </Navbar.Collapse>
+              <Dropdown.Item
+                key="autoscaling"
+                showFullDescription
+                description="   PsyDTrader scales apps to meet user demand, automagically, based on load."
+                icon={icons.scale}
+              >
+                Trading Jornal
+              </Dropdown.Item>
+              <Dropdown.Item
+                key="usage_metrics"
+                showFullDescription
+                description="Real-time metrics to debug issues. Slow query added? We’ll show you exactly where."
+                icon={icons.activity}
+              >
+                Price Alert
+              </Dropdown.Item>
+              <Dropdown.Item
+                key="production_ready"
+                showFullDescription
+                description="   PsyDTrader runs on ACME, join us and others serving requests at web scale."
+                icon={icons.flash}
+              >
+                Traders Tool
+              </Dropdown.Item>
+              <Dropdown.Item
+                key="99_uptime"
+                showFullDescription
+                description="Applications stay on the grid with high availability and high uptime guarantees."
+                icon={icons.server}
+              >
+                +99% Uptime
+              </Dropdown.Item>
+              <Dropdown.Item
+                key="supreme_support"
+                showFullDescription
+                description="Overcome any challenge with a supporting team ready to respond."
+                icon={icons.user}
+              >
+                +Account Reporting
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          <Navbar.Link href="#">Pricing</Navbar.Link>
+          <Navbar.Link href="#"> FAQs </Navbar.Link>
+        </Navbar.Content>
+
+
+
+        <Navbar.Collapse>
+          {collapseItems.map((item, index) => (
+            <Navbar.CollapseItem key={item}>
+              <Link
+                color="inherit"
+                css={{
+                  minWidth: "100%",
+                }}
+                href="#"
+              >
+                {item}
+              </Link>
+            </Navbar.CollapseItem>
+          ))}
+        </Navbar.Collapse>
         <Navbar.Content
 
 
@@ -73,18 +156,34 @@ export default function NavbarItem() {
             },
           }}
         >
+
+
+
           <Navbar.Item>
-          <MyStyledButton
-          className=' bg-[#B89F1B]'
-          auto
-          size="mysize"
-          color="mycolor">
-          Get started
-        </MyStyledButton>
+            <MyStyledButton
+              className=' bg-[#B89F1B]'
+              auto
+              size="mysize"
+            >
+              Sign In
+            </MyStyledButton>
+          </Navbar.Item>
+
+
+          <Navbar.Item>
+            <MyStyledButton
+              className=' bg-[#B89F1B]'
+              auto
+              size="mysize"
+            >
+              Sign Up
+            </MyStyledButton>
           </Navbar.Item>
         </Navbar.Content>
 
       </Navbar>
-    </Layout>
+
+    </Layout >
+
   );
 }
